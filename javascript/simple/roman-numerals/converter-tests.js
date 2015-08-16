52ac45fbb6aece6ecfae5a616049e38454721fbc
Hany Elemary <hany.elemary@gmail.com>
2015-08-16 17:56:11 -0400

    Adding a test and enough code for handling parameter validation for the numberToNumeral on our Converter.

--- javascript/simple/roman-numerals/converter-tests.js
+++ javascript/simple/roman-numerals/converter-tests.js
@@ -1,5 +1,20 @@
-import { Converter } from './converter';
+import Converter from './converter';
 
 describe('Converter:', () => {
-    
+
+	it('throws an error when nothing is passed to the converting function', () => {
+		expect(() => {
+			Converter.numberToNumeral()
+		}).toThrow(
+			new Error('parameter is required and must be a number')
+		);
+	});
+
+	it('throws an error when a non integer param is passed to the converting function', () => {
+		expect(() => {
+			Converter.numberToNumeral("some string")
+		}).toThrow(
+			new Error('parameter is required and must be a number')
+		);
+	});
 });
--- javascript/simple/roman-numerals/converter.js
+++ javascript/simple/roman-numerals/converter.js
@@ -3,6 +3,14 @@
  * This is a converter from a number to numeral and vice versa.
  */
 class Converter {
+    /**
+     * function to convert a number into a roman numeral.
+     * @param  {Integer} number a valid, non-negative integer.
+     * @return {String} a roman numeral.
+     */
+    static numberToNumeral(number) {
+        throw new Error('parameter is required and must be a number');
+    }
 }
 
 export default Converter;
