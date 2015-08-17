9873314ddc8731759718397c57f425c1ef630d82
Hany Elemary <hany.elemary@gmail.com>
2015-08-17 18:08:38 -0400

    Added a validator class to be used in the converter and hide the implementation dteails from the client (converter).

---
+++ javascript/simple/roman-numerals/validator-tests.js
@@ -0,0 +1,23 @@
+import Validator from './validator';
+
+describe('Validator:', () => {	
+	describe('isValidNumber', () => {
+		it('returns false if the number is outside the range [1-3999]', () => {
+			expect(Validator.isValidNumber(-1)).toBe(false);
+			expect(Validator.isValidNumber(0)).toBe(false);
+			expect(Validator.isValidNumber(4000)).toBe(false);			
+		});
+
+		it('returns false if the number is not an integer integer', () => {
+			expect(Validator.isValidNumber(2.5)).toBe(false);
+			expect(Validator.isValidNumber("1")).toBe(false);
+			expect(Validator.isValidNumber('String')).toBe(false);
+		});
+
+		it('returns true if the number is a positive integer within range integer integer', () => {
+			expect(Validator.isValidNumber(1)).toBe(true);
+			expect(Validator.isValidNumber(3999)).toBe(true);
+			expect(Validator.isValidNumber(1988)).toBe(true);
+		});
+	});
+});
---
+++ javascript/simple/roman-numerals/validator.js
@@ -0,0 +1,18 @@
+
+/**
+ * This class represents a validator for the numbers to be converted.
+ * Numbers have to be positive integers between 1 - 3999
+ */
+class Validator {
+
+	/**
+	 * function to determine whether a number is a valid positive integer within range.
+	 * @param  {Integer} number a positive integer.
+	 * @return {Boolean} a true/false.
+	 */
+	static isValidNumber(number) {
+		return (Number.isInteger(number) && (number >= 1 && number <= 3999));
+	}
+}
+
+export default Validator;
